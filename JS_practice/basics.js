55e7f02f1c6d7ab99662ce1e9d5cc7abc8a3cdd5
arowdy98 <arathore98@gmail.com>
2021-07-29 10:11:50 +0530

    Getting Started with JS

---
+++ JS_practice/basics.js
@@ -0,0 +1,13 @@
+a = 21;
+b = a * 2;
+alert(b);
+console.log( b );
+age = prompt( "Please tell me your age:" );
+console.log( age );
+
+const TAX_RATE = 0.08; // 8% sales tax Constants are capitalised
+var amount = 99.99;
+amount = amount * 2;
+amount = amount + (amount * TAX_RATE);
+console.log( amount ); // 215.9784
+console.log( amount.toFixed( 2 ) );
