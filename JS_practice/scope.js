623981bcf352617647182f0d4b354f63e39b101e
arowdy98 <arathore98@gmail.com>
2021-08-02 09:10:20 +0530

    Assignment RWD and JS basics.

--- JS_practice/scope.js
+++ JS_practice/scope.js
@@ -23,3 +23,45 @@ inner();
 console.log( a ); // 1
 }
 outer();
+
+function foo() {
+var a = 1;
+if (a >= 1) {
+let b = 2;
+while (b < 5) {
+let c = b * 2;
+b++;
+console.log( a + c );
+}
+}
+}
+foo();
+// 5 7 9
+//Because of using let instead of var, b will belong only to the if
+//statement and thus not to the whole foo() function’s scope. Simi‐
+//Slarly, c belongs only to the while loop.
+//larly, c belongs only to the while loop.
+
+//Hoisting
+
+var a = 2;
+foo(); // works because `foo()`
+// declaration is "hoisted"
+function foo() {
+a = 3;
+console.log( a ); // 3
+var a; // declaration is "hoisted"
+// to the top of `foo()`
+}
+console.log( a ); // 2
+
+var a = 2;
+foo(); // works because `foo()`
+// declaration is "hoisted"
+function foo() {
+a = 3;
+console.log( a ); // 3
+var a; // declaration is "hoisted"
+// to the top of `foo()`
+}
+console.log( a ); // 2
